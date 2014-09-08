710e6ed17e50a388db2bb6d95d489d0e8d4546b8
Alex Scherbanov <alex@Alexs-MacBook-Pro.local>
2014-09-08 05:06:16 +0400

    deploy.sh now incrementally creates dirs like ~/.files.backup-3

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,35 @@
 #!/usr/bin/env bash
 
-### deploy org-mode --- remove this
-# mkdir -p emacs.d/elisp/
-# cd elisp/
-# git clone git://orgmode.org/org-mode.git
-# cd org-mode/
-# echo 'EMACS	= /Applications/MacPorts/Emacs.app/Contents/MacOS/Emacs' >> local.mk   # TODO: preinit --- must depend on a symlink to this machine local config 
-# git checkout -b stable origin/maint   # http://orgmode.org/worg/org-faq.html -- How can I use a stable release version instead of the bleeding edge master?
-# make up1   # http://orgmode.org/worg/dev/org-build-system.html
+if [[ $1 != "NOW" ]]; then
+    echo "usage: ./deploy.sh NOW"
+    echo
+    echo "will also ~/.files.backup"
+    exit 1
+fi
 
 
-dotfiles="$(dirname "$0")"
-cd $dotfiles
-mkdir -p ~/$dotfiles.backup
 
+cd "$(dirname "$0")"
+backupdir=~/.files.backup
+if [[ -e "$backupdir" ]] ; then
+    i=2
+    while [[ -e "$backupdir-$i" ]] ; do
+        let i++
+    done
+    backupdir="$backupdir-$i"
+fi
+echo "creating dir $backupdir"
+mkdir -p "$backupdir"
 
-for file in `ls -1 | grep -vE '(\..*|README\.md|deploy\.sh)'` ; do 
-    echo $file
-    # mv -v ~/.$file ~/$dotfiles.backup/ 2>/dev/null || true   # for a case if 'set -e'
-    # ln -s $dotfiles/$file ~/.$file
+for file in `ls -1 | grep -vE '(\..*|README\.md|deploy\.sh)'` ; do
+    echo "symlinking ~/.$file" 
+    mv ~/.$file "$backupdir" 2>/dev/null
+    ln -s ~/.files/$file ~/.$file
 done
 
 
 
 
-# git config --global core.excludesfile ~/.gitignore_global
+echo 'my customs'
+git config --global core.excludesfile ~/.gitignore_global
 
